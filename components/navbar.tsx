53ca336ef3ea8fe44252eb50da2842983a8881a6
Khoi Do <khoizpro31@gmail.com>
2024-05-16 00:15:04 +0700

    Add animation with framer-motion in review section

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,10 +2,13 @@ import Link from "next/link";
 import { MaxWidthWrapper } from "./max-width-wrapper";
 import { buttonVariants } from "./ui/button";
 import { ArrowRight } from "lucide-react";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export const Navbar = () => {
-  const user = undefined;
-  const isAdmin = false;
+export const Navbar = async () => {
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  const isAdmin = user?.email === process.env.ADMIN_EMAIL;
 
   return (
     <nav className="sticky z-[100] h-14 inset-x- top-0 w-full border-b border-gray-200 bg-white/75 backdrop-blur-lg transition-all">
@@ -29,7 +32,7 @@ export const Navbar = () => {
                 </Link>
                 {isAdmin ? (
                   <Link
-                    href="/api/auth/logout"
+                    href="/dashboard"
                     className={buttonVariants({
                       size: "sm",
                       variant: "ghost",
