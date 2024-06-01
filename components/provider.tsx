3ca2a94e3ff215944d1dd88ccd02187ed705ecd6
Khoi Do <khoizpro31@gmail.com>
2024-06-01 18:43:45 +0700

    Create action to handle when user purchase but not logged in

--- components/provider.tsx
+++ components/provider.tsx
@@ -1,9 +1,15 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const client = new QueryClient();
 
 export const Provider = ({ children }: { children: React.ReactNode }) => {
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={client}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false}></ReactQueryDevtools>
+    </QueryClientProvider>
+  );
 };
